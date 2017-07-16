24050010168e9cb794a9cedeaa2fa4793f1c73d2
kamlesh <kamal.lakhani56@gmail.com>
2017-07-16 22:46:53 +0530

    ServiceDateTime

---
+++ app/src/main/java/com/jdkgroup/service/ActivityServiceDateTime.java
@@ -0,0 +1,58 @@
+package com.jdkgroup.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.jdkgroup.service.service.BroadcastServiceDateTime;
+
+public class ActivityServiceDateTime extends AppCompatActivity {
+    private static final String TAG = "BroadcastTest";
+    private Intent intent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intent = new Intent(this, BroadcastServiceDateTime.class);
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateUI(intent);
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startService(intent);
+        registerReceiver(broadcastReceiver, new IntentFilter(BroadcastServiceDateTime.BROADCAST_ACTION));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
+        stopService(intent);
+    }
+
+    private void updateUI(Intent intent) {
+        String counter = intent.getStringExtra("counter");
+        String time = intent.getStringExtra("time");
+        Log.d(TAG, counter);
+        Log.d(TAG, time);
+
+        TextView txtDateTime = (TextView) findViewById(R.id.txtDateTime);
+        TextView txtCounter = (TextView) findViewById(R.id.txtCounter);
+        txtDateTime.setText(time);
+        txtCounter.setText(counter);
+    }
+}
